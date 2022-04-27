75495d94c930fc4c441f1e0a78b2f59b188bf7a4
Kasper Timm Hansen <kaspth@gmail.com>
2022-04-27 14:04:11 +0200

    Allow params to be empty

--- lib/active_record/ingress/base.rb
+++ lib/active_record/ingress/base.rb
@@ -5,7 +5,7 @@ class ActiveRecord::Ingress::Base
   require_relative "base/transactions"
   include Transactions # Depends on Callbacks
 
-  def initialize(record, params)
+  def initialize(record, params = {})
     @record, @params = record, params
   end
 
