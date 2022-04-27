031a2052c69b963e2101fab2bab60f1ade422a64
Kasper Timm Hansen <kaspth@gmail.com>
2022-04-27 13:56:01 +0200

    Fix ingress name lookup

--- lib/active_record/ingress/proxy.rb
+++ lib/active_record/ingress/proxy.rb
@@ -7,6 +7,6 @@ class ActiveRecord::Ingress::Proxy < Struct.new(:__record__)
     attr_reader :__record__
 
     def find_record_ingress_named(name)
-      "#{__record__.class.name}::Ingresses::#{name.classify}".constantize
+      "#{__record__.class.name}::Ingresses::#{name.to_s.classify}".constantize
     end
 end
