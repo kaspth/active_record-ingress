470278654dd2ebcfa289aa87cf464bcad510ee20
Kasper Timm Hansen <kaspth@gmail.com>
2022-04-27 13:38:16 +0200

    scaffolding: bundle gem active_record-ingress

---
+++ test/active_record/test_ingress.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class ActiveRecord::TestIngress < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::ActiveRecord::Ingress::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
