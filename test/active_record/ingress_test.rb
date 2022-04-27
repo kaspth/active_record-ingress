61b3d83d9510c1e4e52346fd47228f61584f40ff
Kasper Timm Hansen <kaspth@gmail.com>
2022-04-27 13:56:15 +0200

    Setup initial test harness

--- test/active_record/ingress_test.rb
+++ test/active_record/ingress_test.rb
@@ -3,11 +3,17 @@
 require "test_helper"
 
 class ActiveRecord::IngressTest < Minitest::Test
+  def setup
+    super
+    @post = Post.first
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::ActiveRecord::Ingress::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_performs_update
+    @post.ingressed.update
+    assert_equal "Updated title", @post.title
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "active_record"
 require "active_record/ingress"
 
 require "minitest/autorun"
+
+require_relative "boot/active_record"
+
+author = Author.create!
+author.posts.create! title: "First post"
+
+module Post::Ingresses; end # Zeitwerk handles this in Rails
+
+class Post::Ingresses::Update < ActiveRecord::Ingress::Base
+  def perform
+    post.update title: "Updated title"
+  end
+end
